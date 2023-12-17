3f9d91d773fbfcbb46786dd579d5081497697976
unknown <mudigondagirish97@gmail.com>
2023-12-17 19:25:41 +0530

    10 commit

--- test.java
+++ test.java
@@ -2,10 +2,6 @@ class Hello{
   public static void main(String args[]){
       System.out.println("my first git learning");
       System.out.println("hiii");
-<<<<<<< HEAD
       -------
-=======
-      1111
->>>>>>> f042aba66219bf3bc8364ad9ca9529221244b67e
   }
 }
